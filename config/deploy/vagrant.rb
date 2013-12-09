4a7c4be518065fbfc64155d4a19c2dbbb5bdd5b2
Nick Muerdter <nick.muerdter@nrel.gov>
2013-12-08 20:56:54 -0700

    Another try at automatic vagrant deploys.

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,5 +1,3 @@
-require "capistrano_nrel_ext/recipes/vagrant_deploy"
-
 # Set the servers for this stage.
 role :app, "api.vagrant"
 role :web, "api.vagrant"
@@ -13,9 +11,5 @@ set :rails_env, "development"
 # Set gem bundler options for the development environment.
 set :bundle_without, [:test]
 set :bundle_flags, "--quiet"
-
-# Reset forced "vagrant" user. Deploy as the DEPLOY_USER env variable or fall
-# back to the running user.
-set :user, ENV["DEPLOY_USER"]
 
 default_environment["SWAGGER_HOST"] = "http://api.vagrant"
