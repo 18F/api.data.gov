009fc231331f5cd265cb4ccf0a2f3f3954f085aa
Nick Muerdter <nick.muerdter@nrel.gov>
2016-12-10 09:24:38 -0700

    Update to Middleman v4.

--- Capfile
+++ Capfile
@@ -1,4 +1,8 @@
-load 'deploy' if respond_to?(:namespace) # cap2 differentiator
-Dir['vendor/gems/*/recipes/*.rb','vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
+# Load DSL and Setup Up Stages
+require "capistrano/setup"
 
-load 'config/deploy' # remove this line to skip loading any of the default tasks
+# Includes default deployment tasks
+require "capistrano/deploy"
+
+# Loads custom tasks from `lib/capistrano/tasks" if you have any defined.
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config.rb
+++ config.rb
@@ -1,46 +1,53 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
+
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# page "/path/to/file.html", layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 page "/404.html", :directory_index => false
+
+# General configuration
+
+activate :sprockets
+activate :directory_indexes
+activate :syntax
+activate :relative_assets
+
+set :css_dir, "static/stylesheets"
+set :js_dir, "static/javascripts"
+set :fonts_dir, "static/fonts"
+set :images_dir, "static/img"
+
+set :markdown_engine, :kramdown
+set :markdown, {
+  :input => "GFM",
+  :smart_quotes => ["apos", "apos", "quot", "quot"],
+}
+
+# Reload the browser automatically whenever files change
+configure :development do
+  if(ENV["MIDDLEMAN_LIVERELOAD_PORT"] && ENV["MIDDLEMAN_LIVERELOAD_JS_HOST"])
+    activate :livereload, :port => ENV["MIDDLEMAN_LIVERELOAD_PORT"], :js_host => ENV["MIDDLEMAN_LIVERELOAD_JS_HOST"]
+  else
+    activate :livereload
+  end
+end
 
 ###
 # Helpers
 ###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
 
 # Methods defined in the helpers block are available in templates
 helpers do
@@ -58,14 +65,9 @@ helpers do
   end
 end
 
-set :css_dir, 'static/stylesheets'
-set :js_dir, 'static/javascripts'
-set :fonts_dir, 'static/fonts'
-set :images_dir, 'static/img'
-
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+  # Minify CSS on build
   activate :minify_css
 
   # Minify Javascript on build
@@ -81,26 +83,7 @@ configure :build do
     # be hardcoded.
     %r{signup_embed.js},
   ]
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-
-  # Redirects
-  activate :alias
 end
-
-activate :directory_indexes
-
-activate :syntax
-
-set :markdown_engine, :kramdown
-set :markdown, {
-  :input => 'GFM',
-  :smart_quotes => ['apos', 'apos', 'quot', 'quot'],
-}
 
 after_configuration do
   sprockets.append_path(File.join(root, "vendor/data.gov/themes/roots-nextdatagov/assets/css"))
@@ -129,8 +112,3 @@ end
 if(build?)
   ENV["WEB_SITE_ROOT"] ||= "https://api.data.gov"
 end
-
-# Prevent vim temp files from triggering file watching business.
-set :file_watcher_ignore, [
-  /.*\.swp$/
-]
