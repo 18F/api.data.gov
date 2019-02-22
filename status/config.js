c6f60c15dae2ae12a81a92a10e54b3fe21bf50df
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-02-22 14:49:13 -0700

    Initial playing around with a status page.

---
+++ status/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "title": "api.data.gov System Status",
+  "name": "api.data.gov_system_status",
+  "description": "Somet description text",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml",
+    "systems": [
+      "gateway",
+      "api",
+      "admin",
+    ],
+  }
+}
