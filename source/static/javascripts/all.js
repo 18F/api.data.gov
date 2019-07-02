23d20f6c9fba92d739eb976c91a0317a676195ca
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-07-02 15:56:38 -0600

    Remove unused JS dependencies.
    With the new metrics page live, these dependencies are now unused.

--- source/static/javascripts/all.js
+++ source/static/javascripts/all.js
@@ -1,7 +1,5 @@
 //= require _datagov
 //= require _vendor/jquery.tableofcontents.js
-//= require numeral
-//= require moment
 
 $(document).ready(function() {
   $('#toc').tableOfContents(document.body, {
